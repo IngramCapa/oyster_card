3439de325e2c2792ce061a29f805210504d55152
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-20 16:19:21 +0000

    add station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+
+describe Station do
+
+  let(:subject) {Station.new("holburn", 1)}
+
+  it "should have a name" do
+    expect(subject).to respond_to(:name) 
+  end
+
+  it "should have a zone" do
+    expect(subject).to respond_to(:zone) 
+  end
+
+end
